2c931a2f98e3afa621b46ab1575d1b9d0b966793
Oliver Geisel <geisel.oliver@googlemail.com>
2023-05-18 12:54:17 +0200

    Merge and format files. Fix Bug in C++ benchmark uncomment throw of exception

--- bench/bench-c.c
+++ bench/bench-c.c
@@ -66,108 +66,109 @@ const int E_All( const int i ) {
 	return FEHLER;
 }
 
-int run_All(struct timespec *results) {
-  srand(time(NULL));
-  const int i = rand()%2;
-  struct timespec start, end;
+int run_All( struct timespec *results ) {
+	srand( time( NULL ));
+	const int i = rand() % 2;
+	struct timespec start, end;
 
-  // warm up
-  int run;
-  for (run = 0; run < WARMUP; ++run) {
-    if (A_All(i) < FEHLER-1) {
-      perror("There was an error!\n");
-    }
-  }
+	// warm up
+	int run;
+	for (run = 0; run < WARMUP; ++run) {
+		if (A_All( i ) < FEHLER - 1) {
+			perror( "There was an error!\n" );
+		}
+	}
 
-  // REAL run
-  for (run = 0; run < MEASURE; ++run) {
-    start = gtod();
-    int error = A_All(i);
-    if (error < FEHLER-1) {
-      perror("There was an error!\n");
-    }
-    end = gtod();
-    results[run * 2] = start;
-    results[run * 2 + 1] = end;
-  }
-  return 0;
+	// REAL run
+	for (run = 0; run < MEASURE; ++run) {
+		start = gtod();
+		int error = A_All( i );
+		if (error < FEHLER - 1) {
+			perror( "There was an error!\n" );
+		}
+		end = gtod();
+		results[run * 2] = start;
+		results[run * 2 + 1] = end;
+	}
+	return 0;
 }
 
 // ---------------------------------------------------------------------------------------------
 
-const int A_First(const int i) {
-   void* pointer = malloc(2);
-  int error = B_First(i);
-  if (error == BESONDERER_FEHLER) {
-    // printf("Fehler gefangen in A\n");
-    return KEIN_FEHLER;
-  }
-  if (error < 0) {
-    printf("Es gab einen anderen Fehler!\n");
-  }
-  free(pointer);
-  return 1;
+const int A_First( const int i ) {
+	void *pointer = malloc( 2 );
+	int error = B_First( i );
+	if (error == BESONDERER_FEHLER) {
+		// printf("Fehler gefangen in A\n");
+		return KEIN_FEHLER;
+	}
+	if (error < 0) {
+		printf( "Es gab einen anderen Fehler!\n" );
+	}
+	free( pointer );
+	return 1;
 }
 
-const int B_First(const int i) {
-  void* pointer = malloc(2);
-  if (C_First(i) <= FEHLER) {
-    printf("Fehler gefangen in B\n");
-    return FEHLER-i;
-  }
-  free(pointer);
-  return 0;
+const int B_First( const int i ) {
+	void *pointer = malloc( 2 );
+	if (C_First( i ) <= FEHLER) {
+		printf( "Fehler gefangen in B\n" );
+		return FEHLER - i;
+	}
+	free( pointer );
+	return 0;
 }
 
-const int C_First(const int i) {
-   void* pointer = malloc(2);
-  if (D_First(i) <= FEHLER) {
-    printf("Fehler gefangen in C\n");
-    return FEHLER-i;
-  }
-  free(pointer);
-  return 0;
+const int C_First( const int i ) {
+	void *pointer = malloc( 2 );
+	if (D_First( i ) <= FEHLER) {
+		printf( "Fehler gefangen in C\n" );
+		return FEHLER - i;
+	}
+	free( pointer );
+	return 0;
 }
 
-const int D_First(const int i) {
-  void* pointer = malloc(2);
-  if (E_First(i) <= FEHLER) {
-    printf("Fehler gefangen in D\n");
-    return FEHLER-i;
-  }
-  free(pointer);
-  return 0;
+const int D_First( const int i ) {
+	void *pointer = malloc( 2 );
+	if (E_First( i ) <= FEHLER) {
+		printf( "Fehler gefangen in D\n" );
+		return FEHLER - i;
+	}
+	free( pointer );
+	return 0;
 }
 
-const int E_First(const int i) {
-  void* pointer = malloc(2);
-  free(pointer);
-return BESONDERER_FEHLER; }
+const int E_First( const int i ) {
+	void *pointer = malloc( 2 );
+	free( pointer );
+	return BESONDERER_FEHLER;
+}
 
-int run_First(struct timespec *results) {
-  srand(time(NULL));
-  const int i = rand()%2;
-  struct timespec start, end;
+int run_First( struct timespec *results ) {
+	srand( time( NULL ));
+	const int i = rand() % 2;
+	struct timespec start, end;
 
-  // warm up
-  int run;
-  for (run = 0; run < WARMUP; ++run) {
-    if (A_First(i) < FEHLER-1) {
-      perror("There was an error!\n");
-    }
-  }
+	// warm up
+	int run;
+	for (run = 0; run < WARMUP; ++run) {
+		if (A_First( i ) < FEHLER - 1) {
+			perror( "There was an error!\n" );
+		}
+	}
 
-  // REAL run
-  for (run = 0; run < MEASURE; ++run) {
-    start = gtod();
-    if (A_First(i) < FEHLER-1) {
-      perror("There was an error!\n");
-    }
-    end = gtod();
-    results[run * 2] = start;
-    results[run * 2 + 1] = end;
-  }
-  return 0;
+	// REAL run
+	for (run = 0; run < MEASURE; ++run) {
+		start = gtod();
+		if (A_First( i ) < FEHLER - 1) {
+			perror( "There was an error!\n" );
+		}
+		end = gtod();
+		results[run * 2] = start;
+		results[run * 2 + 1] = end;
+	}
+	return 0;
 }
 
 // ---------------------------------------------------------------------------------------------
@@ -203,10 +204,10 @@ void evaluate( struct timespec *results, struct timespec start,
 	}
 	variance = temp / MEASURE;
 	double standard_deviation = sqrt( variance );
-	printf( "Gesamte Zeit:\t%.8fs\t%fms\n", pres_diff, pres_diff * 1000 );
+	printf( "Gesamte Zeit:\t\t%.8fs\t%fms\n", pres_diff, pres_diff * 1000 );
 	printf( "Durchschnittliche Zeit:\t%.6fs\t%fms\t%fmikrosec.\n",
 	        expectaion_value, expectaion_value * 1000, expectaion_value * 1000000 );
-	printf( "Varianze:\t%.6fs\t%fms\t%fmikrosec.\n", variance, variance * 1000,
+	printf( "Varianze:\t\t%.6fs\t%fms\t%fmikrosec.\n", variance, variance * 1000,
 	        variance * 1000000 );
 	printf( "Standardabweichung:\t%.6fs\t%fms\n", standard_deviation,
 	        standard_deviation * 1000 );
